8fa55e12e0757ad516ebb50ce090e70d6bb4e6a9
Trisout <81978700+trisout78@users.noreply.github.com>
2024-03-08 19:51:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 apt update
 apt install -y pip nodejs nano
-npm install -g npm@latest
 systemctl stop apache2
 apt remove apache2
 apt autoremove
